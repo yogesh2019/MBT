49c730c811206471717b1fb38c5e18f85ad8b885
yogesh2019 <yogeshkkhichi@gmail.com>
2020-10-10 12:25:19 +0530

    day1

---
+++ app.js
@@ -0,0 +1,42 @@
+function myFunction(){
+    console.log("hi");
+    var x = document.getElementById("mydiv");
+    if(x.style.display  === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function myFunction1(){
+    console.log("hi");
+    var x = document.getElementById("mydiv1");
+    if(x.style.display  === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function myFunction2(){
+    console.log("hi");
+    var x = document.getElementById("mydiv2");
+    if(x.style.display  === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+
+function myFunction3(){
+    console.log("hi");
+    var x = document.getElementById("mydiv3");
+    if(x.style.display  === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+
